771d29d6196c20b1a796e8f9d52f519d1022ee40
MahmudAlj <96774345+MahmudAlj@users.noreply.github.com>
2024-11-12 19:47:17 +0300

    update code data types

--- app/src/main/java/com/example/evreything_about_kotlin/main.kts
+++ app/src/main/java/com/example/evreything_about_kotlin/main.kts
@@ -1,2 +1,15 @@
 println("hello world")
+print("merhaba mahmud")
 
+var age = 20 
+val name = "mahmud" 
+
+// data types
+var hight : Float = 1.81 
+val last_name : String = "code" 
+var monay : Double = 22.654
+val favo : Character = 't'
+var yes : Boolean = True 
+val ok : Integer = 4
+
+//veri turleri
